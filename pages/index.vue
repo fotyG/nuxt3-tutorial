a850fe768c2f5e11f9eb72545c4538c744457f4f
Sota Giorgadze <giorgadze@inbox.lv>
2023-09-27 20:49:38 +0200

    init

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nisi
+      deserunt saepe eligendi? Est quo voluptatum odit odio illum fugit.
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nisi
+      deserunt saepe eligendi? Est quo voluptatum odit odio illum fugit.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
